a160c7c26d067a46a55f422b05fa8a03723e3fe8
Tabaxco <rodriguesgui48@gmail.com>
2026-02-19 15:58:52 -0300

    refactor: removed all throws statements from GenericDAO interface

--- src/main/java/controller/dao/GenericDAO.java
+++ src/main/java/controller/dao/GenericDAO.java
@@ -3,8 +3,8 @@ package controller.dao;
 import java.sql.SQLException;
 
 public interface GenericDAO<T> {
-    void insert(T entity) throws SQLException;
-    T findById(int id) throws SQLException;
-    void update(T entity) throws SQLException;
-    void delete(int id) throws SQLException;
+    void insert(T entity);
+    T findById(int id);
+    void update(T entity);
+    void delete(int id);
 }
